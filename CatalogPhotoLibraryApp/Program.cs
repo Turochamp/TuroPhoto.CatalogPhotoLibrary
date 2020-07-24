31da47869e840143bd30ea7f6e0cb0da1efabdac
Michael Ahs <michael@creatus.se>
2020-07-24 16:34:30 +0200

    Updated git ignore

---
+++ CatalogPhotoLibraryApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TuroPhoto.CatalogPhotoLibraryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
